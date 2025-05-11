022272ea7cf6a659a53ca2aeb4abe36534b2a2a6
iakona <1824122+iakona@users.noreply.github.com>
2025-05-11 11:09:33 -0500

    You can now use Q and E on tokens to subtract or add 1

---
+++ objects/3065c9/contained/bb3ab8/script.lua
@@ -0,0 +1,32 @@
+local bagName = "harmBag"
+
+function tryRotate(spin, flip, _, old_spin, old_flip)
+    if flip - old_flip ~= 0 then
+        return true
+    end
+
+    local spinDiff = spin - old_spin
+    if spinDiff > 180 then
+        spinDiff = spinDiff - 360
+    elseif spinDiff < -180 then
+        spinDiff = spinDiff + 360
+    end
+
+    local quantity = self.getQuantity()
+    if quantity == -1 then
+        quantity = 1
+    end
+
+    if spinDiff < 0 then
+        if quantity == 1 then
+            self.destruct()
+        else
+            self.takeObject().destruct()
+        end
+    else
+        local bag = Global.getVar(bagName)
+        bag.takeObject({position = self.getPosition(), rotation = self.getRotation(), smooth = false})
+    end
+
+    return false
+end
