9c988f37a4ab956b59aba6162c4b03a57342ba22
iakona <1824122+iakona@users.noreply.github.com>
2025-05-11 11:30:32 -0500

    Switched path card search box to not need exact match and will instead grab first card which starts the same

--- objects/deb4bb/script.lua
+++ objects/deb4bb/script.lua
@@ -1,19 +1,20 @@
 function onLoad(_)
-    self.UI.setAttribute("input", "tooltip", "Enter Path Card to grab")
+    self.UI.setAttribute("input", "tooltip", "Enter Path Card to grab.\nThis looks for first card which\nstarts the same as your input")
 end
 
+-- For path cards should be safe enough to just match start of card name with input rather than requiring full name of card
 function GrabPath(params)
     local foundCard = nil
     local lowerInput = params.path:lower()
     for _, deck in pairs(self.getData().ContainedObjects) do
-        if deck.Nickname:lower() == lowerInput then
+        if deck.Nickname:sub(1, #lowerInput):lower() == lowerInput then
             foundCard = self.takeObject({guid = deck.GUID, position = params.position})
 
             self.UI.setAttribute("input", "text", "")
             break
         end
         for _, card in pairs(deck.ContainedObjects) do
-            if card.Nickname:lower() == lowerInput then
+            if card.Nickname:sub(1, #lowerInput):lower() == lowerInput then
                 local obj = self.takeObject({guid = deck.GUID})
                 foundCard = obj.takeObject({guid = card.GUID, position = params.position})
                 self.putObject(obj)
