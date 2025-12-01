40f90ae80d6bdf03fc8c99c97937c0e73d473a3f
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2025-11-30 23:30:20 -0500

    add hotkeys for dice

--- script.lua
+++ script.lua
@@ -25,6 +25,9 @@ energyBags = {
 harmBag = "3065c9"
 progressBag = "3bdc5a"
 allPurposeBag = "44520f"
+harmDiceBag = "5ec964"
+progressDiceBag = "90f0ff"
+allPurposeDiceBag = "a0263e"
 
 playerBoards = {
     ["Red"] = "ce80f5",
@@ -117,6 +120,9 @@ function onLoad(data)
     harmBag = getObjectFromGUID(harmBag)
     progressBag = getObjectFromGUID(progressBag)
     allPurposeBag = getObjectFromGUID(allPurposeBag)
+    harmDiceBag = getObjectFromGUID(harmDiceBag)
+    progressDiceBag = getObjectFromGUID(progressDiceBag)
+    allPurposeDiceBag = getObjectFromGUID(allPurposeDiceBag)
     for key, guid in pairs(playerBoards) do
         playerBoards[key] = getObjectFromGUID(guid)
     end
@@ -169,6 +175,15 @@ function onLoad(data)
     end)
     addHotkey("Spawn All-Purpose Token", function (playerColor, hoveredObject, cursorLocation, key_down_up)
         allPurposeBag.takeObject({position = cursorLocation + Vector(0, 2, 0), rotation = Vector(0, 180, 0)})
+    end)
+    addHotkey("Spawn Harm Die", function (playerColor, hoveredObject, cursorLocation, key_down_up)
+        harmDiceBag.takeObject({position = cursorLocation + Vector(0, 2, 0), rotation = Vector(0, 180, 0)})
+    end)
+    addHotkey("Spawn Progress Die", function (playerColor, hoveredObject, cursorLocation, key_down_up)
+        progressDiceBag.takeObject({position = cursorLocation + Vector(0, 2, 0), rotation = Vector(0, 180, 0)})
+    end)
+    addHotkey("Spawn All-Purpose Die", function (playerColor, hoveredObject, cursorLocation, key_down_up)
+        allPurposeDiceBag.takeObject({position = cursorLocation + Vector(0, 2, 0), rotation = Vector(0, 180, 0)})
     end)
 
     addHotkey("Draw Path Card", function (playerColor, hoveredObject, cursorLocation, key_down_up)
